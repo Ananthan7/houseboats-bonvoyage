5dbf4ba5f82f58486a1085318b3d33c3f18be8c4
Ananthan7 <ananthankrishnan77@gmail.com>
2022-01-22 23:10:45 +0530

    modified second phase

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,6 @@
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
-
     if(toggle && nav){
         toggle.addEventListener('click', ()=>{
             nav.classList.toggle('show')
@@ -10,21 +9,17 @@ const showMenu = (toggleId, navId) =>{
     }
 }
 showMenu('nav-toggle','nav-menu')
-
 /*===== ACTIVE AND REMOVE MENU =====*/
 const navLink = document.querySelectorAll('.nav__link');   
-
 function linkAction(){
   /*Active link*/
   navLink.forEach(n => n.classList.remove('active'));
   this.classList.add('active');
-  
   /*Remove menu mobile*/
   const navMenu = document.getElementById('nav-menu')
   navMenu.classList.remove('show')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
-
 /*===== SCROLL REVEAL ANIMATION =====*/
 const sr = ScrollReveal({
     origin: 'top',
@@ -32,68 +27,47 @@ const sr = ScrollReveal({
     duration: 2000,
     reset: true
 });
-
 /*SCROLL HOME*/
 sr.reveal('.home__title',{}); 
 sr.reveal('.button',{delay: 200}); 
 sr.reveal('.home__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
-
 /*SCROLL ABOUT*/
 sr.reveal('.about__img',{}); 
 sr.reveal('.about__subtitle',{delay: 400}); 
 sr.reveal('.about__text',{delay: 400}); 
-
 /*SCROLL SKILLS*/
 sr.reveal('.skills__subtitle',{}); 
 sr.reveal('.skills__text',{}); 
 sr.reveal('.skills__data',{interval: 200}); 
 sr.reveal('.skills__img',{delay: 600});
-
-// /*SCROLL WORK*/
-// sr.reveal('.work__img',{interval: 200}); 
-
-// /*SCROLL CONTACT*/
-// sr.reveal('.contact__input',{interval: 200}); 
-
 /* ======== image slider starts========= */
-
 const slides = document.querySelectorAll('.slide');
 const controls = document.querySelectorAll('.control');
 let activeSlide = 0;
 let prevActive = 0;
-
 changeSlides();
 let int = setInterval(changeSlides, 4000);
-
 function changeSlides() {
 	slides[prevActive].classList.remove('active');
 	controls[prevActive].classList.remove('active');
-
 	slides[activeSlide].classList.add('active');
 	controls[activeSlide].classList.add('active');
-	
 	prevActive = activeSlide++;
-	
 	if(activeSlide >= slides.length) {
 		activeSlide = 0;
 	}
-	
 	console.log(prevActive, activeSlide);
 }
-
 controls.forEach(control => {
 	control.addEventListener('click', () => {
 		let idx = [...controls].findIndex(c => c === control);
 		activeSlide = idx;
-
 		changeSlides();
-
 		clearInterval(int);
 		int = setInterval(changeSlides, 6000);
 	});
 });
-
 /* ======== image slider ends========= */
 
 
